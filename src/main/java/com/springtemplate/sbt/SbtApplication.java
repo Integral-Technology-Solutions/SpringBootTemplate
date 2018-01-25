12bf3a4061bae0b4f03b5776720cf6a2e4aba6fc
Callum <cschneid@integraltech.com.au>
2018-01-25 15:00:54 +1000

    Modified dependencies

--- src/main/java/com/springtemplate/sbt/SbtApplication.java
+++ src/main/java/com/springtemplate/sbt/SbtApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SbtApplication {
 
+	/*
+	This is used to start the application
+	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(SbtApplication.class, args);
 	}
